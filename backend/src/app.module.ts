e585ad9e64a55160245a1981b6f0a587c1585fed
handryos <handryosgds12@hotmail.com>
2025-07-25 14:21:25 -0300

    feat: add the modules dependencies of auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { databaseConfig } from './infra/config/database.config';
 import { BaseModule } from './modules/base.module';
+import { JwtAuthGuard } from './infra/guards/jwt-auth.guard';
 
 @Module({
   imports: [SequelizeModule.forRoot(databaseConfig), BaseModule],
   controllers: [AppController],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- backend/src/modules/base.module.ts
+++ backend/src/modules/base.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CoffeeModule } from './coffe/coffee.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CoffeeModule],
-  exports: [CoffeeModule],
+  imports: [CoffeeModule, AuthModule],
+  exports: [CoffeeModule, AuthModule],
 })
 export class BaseModule {}
