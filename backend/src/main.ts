643d2cf99d909edebf7f360a198f21789f9e973b
handryos <handryos12@gmail.com>
2025-07-28 10:53:09 -0300

    fix: add the method to enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: process.env.FRONTEND_URL,
-    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
   app.useGlobalPipes(
